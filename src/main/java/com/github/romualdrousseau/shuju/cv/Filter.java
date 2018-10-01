61234d332645b5daccd3fa161c364f27d45bad69
Romuald Rousseau <romualdrousseau@gmail.com>
2018-10-02 02:04:52 +0800

    Add a negation filter; need to find a better formalism

--- src/main/java/com/github/romualdrousseau/shuju/cv/Filter.java
+++ src/main/java/com/github/romualdrousseau/shuju/cv/Filter.java
@@ -27,5 +27,26 @@ public class Filter
 		}
 	}
 
+	public void applyNeg(ISearchBitmap searchBitmap, int threshold) {
+		int hw = filter.getWidth() / 2;
+		int hh = filter.getHeight() / 2;
+
+		for(int y = 0; y < searchBitmap.getHeight(); y++) {
+			for(int x = 0; x < searchBitmap.getWidth(); x++) {
+
+				int acc = 0;
+				for(int i = 0; i < this.filter.getHeight(); i++) {
+					for(int j = 0; j < this.filter.getWidth(); j++) {
+						acc += this.filter.get(j, i) * searchBitmap.get(x + j - hw, y + i - hh);
+					}
+				}
+
+				if(acc >= threshold) {
+					searchBitmap.set(x, y, 1);
+				}
+			}
+		}
+	}
+
 	private Template filter;
 }
