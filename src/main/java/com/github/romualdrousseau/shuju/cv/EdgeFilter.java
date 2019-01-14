780ea831ef22530e483fad193a0ab29492bea253
Romuald Rousseau <romualdrousseau@gmail.com>
2019-01-14 19:40:55 +0800

    Add  filter with clip aurgument

--- src/main/java/com/github/romualdrousseau/shuju/cv/EdgeFilter.java
+++ src/main/java/com/github/romualdrousseau/shuju/cv/EdgeFilter.java
@@ -2,52 +2,41 @@ package com.github.romualdrousseau.shuju.cv;
 
 public class EdgeFilter
 {
-	public EdgeFilter() {
-    }
-
-    public void apply(ISearchBitmap searchBitmap, double threshold) {
-		for(int y = 0; y < searchBitmap.getHeight(); y++) {
-			for(int x = 0; x < searchBitmap.getWidth(); x++) {
-				int lx = this.edgeX.sobel(searchBitmap, x, y);
-                int ly = this.edgeY.sobel(searchBitmap, x, y);
-                double acc = Math.sqrt(lx * lx + ly * ly);
-                //double phi = Math.atan2(ly, lx);
-				if(acc < threshold) {
-					searchBitmap.set(x, y, 0);
-				}
-			}
-		}
-    }
-
-    public void applyNeg(ISearchBitmap searchBitmap, double threshold) {
-		for(int y = 0; y < searchBitmap.getHeight(); y++) {
-			for(int x = 0; x < searchBitmap.getWidth(); x++) {
-				int lx = this.edgeX.sobel(searchBitmap, x, y);
-                int ly = this.edgeY.sobel(searchBitmap, x, y);
-                double acc = Math.sqrt(lx * lx + ly * ly);
-                //double phi = Math.atan2(ly, lx);
-				if(acc >= threshold) {
-					searchBitmap.set(x, y, 1);
-				}
-			}
-		}
+    public EdgeFilter() {
     }
 
     public void apply(ISearchBitmap sourceBitmap, ISearchBitmap destBitmap, double threshold) {
-		for(int y = 0; y < sourceBitmap.getHeight(); y++) {
-			for(int x = 0; x < sourceBitmap.getWidth(); x++) {
+        for(int y = 0; y < sourceBitmap.getHeight(); y++) {
+            for(int x = 0; x < sourceBitmap.getWidth(); x++) {
                 int lx = this.edgeX.sobel(sourceBitmap, x, y);
                 int ly = this.edgeY.sobel(sourceBitmap, x, y);
                 double acc = Math.sqrt(lx * lx + ly * ly);
                 //double phi = Math.atan2(ly, lx);
-				if(acc < threshold) {
-					destBitmap.set(x, y, 0);
+                if(acc < threshold) {
+                    destBitmap.set(x, y, 0);
                 }
                 else {
                     destBitmap.set(x, y, 1);
                 }
-			}
-		}
+            }
+        }
+    }
+
+    public void apply(ISearchBitmap sourceBitmap, ISearchBitmap destBitmap, int[] clip, double threshold) {
+        for(int y = clip[1]; y < clip[3]; y++) {
+            for(int x = clip[0]; x < clip[2]; x++) {
+                int lx = this.edgeX.sobel(sourceBitmap, x, y);
+                int ly = this.edgeY.sobel(sourceBitmap, x, y);
+                double acc = Math.sqrt(lx * lx + ly * ly);
+                //double phi = Math.atan2(ly, lx);
+                if(acc < threshold) {
+                    destBitmap.set(x, y, 0);
+                }
+                else {
+                    destBitmap.set(x, y, 1);
+                }
+            }
+        }
     }
 
     private Template edgeX = new Template(new int[][] {
--- src/main/java/com/github/romualdrousseau/shuju/cv/Filter.java
+++ src/main/java/com/github/romualdrousseau/shuju/cv/Filter.java
@@ -2,45 +2,81 @@ package com.github.romualdrousseau.shuju.cv;
 
 public class Filter
 {
-	public Filter(Template filter) {
-    	this.filter = filter;
-  	}
+    public Filter(Template filter) {
+        this.filter = filter;
+    }
 
-	public void apply(ISearchBitmap searchBitmap, double threshold) {
-		for(int y = 0; y < searchBitmap.getHeight(); y++) {
-			for(int x = 0; x < searchBitmap.getWidth(); x++) {
+    public void apply(ISearchBitmap searchBitmap, double threshold) {
+        for(int y = 0; y < searchBitmap.getHeight(); y++) {
+            for(int x = 0; x < searchBitmap.getWidth(); x++) {
                 int acc = this.filter.sobel(searchBitmap, x, y);
-				if(acc < threshold) {
-					searchBitmap.set(x, y, 0);
-				}
-			}
-		}
-	}
+                if(acc < threshold) {
+                    searchBitmap.set(x, y, 0);
+                }
+            }
+        }
+    }
 
-	public void applyNeg(ISearchBitmap searchBitmap, double threshold) {
-		for(int y = 0; y < searchBitmap.getHeight(); y++) {
-			for(int x = 0; x < searchBitmap.getWidth(); x++) {
-				int acc = this.filter.sobel(searchBitmap, x, y);
-				if(acc >= threshold) {
-					searchBitmap.set(x, y, 1);
-				}
-			}
-		}
+    public void apply(ISearchBitmap searchBitmap, int[] clip, double threshold) {
+        for(int y = clip[1]; y < clip[3]; y++) {
+            for(int x = clip[0]; x < clip[2]; x++) {
+                int acc = this.filter.sobel(searchBitmap, x, y);
+                if(acc < threshold) {
+                    searchBitmap.set(x, y, 0);
+                }
+            }
+        }
+    }
+
+    public void applyNeg(ISearchBitmap searchBitmap, double threshold) {
+        for(int y = 0; y < searchBitmap.getHeight(); y++) {
+            for(int x = 0; x < searchBitmap.getWidth(); x++) {
+                int acc = this.filter.sobel(searchBitmap, x, y);
+                if(acc >= threshold) {
+                    searchBitmap.set(x, y, 1);
+                }
+            }
+        }
+    }
+
+    public void applyNeg(ISearchBitmap searchBitmap, int[] clip, double threshold) {
+        for(int y = clip[1]; y < clip[3]; y++) {
+            for(int x = clip[0]; x < clip[2]; x++) {
+                int acc = this.filter.sobel(searchBitmap, x, y);
+                if(acc >= threshold) {
+                    searchBitmap.set(x, y, 1);
+                }
+            }
+        }
     }
 
     public void apply(ISearchBitmap sourceBitmap, ISearchBitmap destBitmap, double threshold) {
         for(int y = 0; y < sourceBitmap.getHeight(); y++) {
-			for(int x = 0; x < sourceBitmap.getWidth(); x++) {
-				int acc = this.filter.sobel(sourceBitmap, x, y);
-				if(acc < threshold) {
-					destBitmap.set(x, y, 0);
+            for(int x = 0; x < sourceBitmap.getWidth(); x++) {
+                int acc = this.filter.sobel(sourceBitmap, x, y);
+                if(acc < threshold) {
+                    destBitmap.set(x, y, 0);
                 }
                 else {
                     destBitmap.set(x, y, 1);
                 }
-			}
-		}
+            }
+        }
     }
 
-	private Template filter;
+    public void apply(ISearchBitmap sourceBitmap, ISearchBitmap destBitmap, int[] clip, double threshold) {
+        for(int y = clip[1]; y < clip[3]; y++) {
+            for(int x = clip[0]; x < clip[2]; x++) {
+                int acc = this.filter.sobel(sourceBitmap, x, y);
+                if(acc < threshold) {
+                    destBitmap.set(x, y, 0);
+                }
+                else {
+                    destBitmap.set(x, y, 1);
+                }
+            }
+        }
+    }
+
+    private Template filter;
 }
