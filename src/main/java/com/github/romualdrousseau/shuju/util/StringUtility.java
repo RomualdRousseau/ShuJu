dbc0ce8500c6e06629a8c5494b6af5b518f161b8
Romuald Rousseau <romualdrousseau@gmail.com>
2022-12-30 12:20:33 +0800

    Refacor

--- src/main/java/com/github/romualdrousseau/shuju/util/StringUtility.java
+++ src/main/java/com/github/romualdrousseau/shuju/util/StringUtility.java
@@ -2,9 +2,8 @@ package com.github.romualdrousseau.shuju.util;
 
 import java.util.regex.Pattern;
 
-public class StringUtility
-{
-	public static final String WHITE_SPACES = "\\s\\u00A0\\u3000";
+public class StringUtility {
+    public static final String WHITE_SPACES = "\\s\\u00A0\\u3000";
 
     public static final String WRONG_UNICODE = "\\uFFFD";
 
@@ -14,103 +13,116 @@ public class StringUtility
 
     public static final char BOM_CHAR = '\uFEFF';
 
-    public static boolean isEmpty(String s) {
+    public static boolean isEmpty(final String s) {
         return s == null || StringUtility.trim(s).equals("");
     }
 
-    public static boolean isFastEmpty(String s) {
+    public static boolean isFastEmpty(final String s) {
         return s == null || s.trim().isEmpty();
     }
 
-	public static String trim(String s) {
-		return trim(s, StringUtility.WHITE_SPACES);
-	}
-
-	public static String trim(String s, String whiteSpaces) {
-		if(s == null) {
-			return null;
-		}
-		return s.replaceAll("^[" + whiteSpaces + "]+", "").replaceAll("[" +whiteSpaces + "]+$", "");
-	}
-
-	public static String normalizeWhiteSpaces(String s) {
-		return normalizeWhiteSpaces(s, StringUtility.WHITE_SPACES);
-	}
-
-	public static String normalizeWhiteSpaces(String s, String whiteSpaces) {
-		if(s == null) {
-			return null;
-		}
-		return s.replaceAll("[" + whiteSpaces + "]", " ");
-	}
-
-	public static String removeWhiteSpaces(String s) {
-		return removeWhiteSpaces(s, StringUtility.WHITE_SPACES);
-	}
-
-	public static String removeWhiteSpaces(String s, String whiteSpaces) {
-		if(s == null) {
-			return null;
-		}
-		return s.replaceAll("[" + whiteSpaces + "]", "");
-	}
-
-	public static String singleWhiteSpaces(String s) {
-		return singleWhiteSpaces(s, StringUtility.WHITE_SPACES);
-	}
-
-	public static String singleWhiteSpaces(String s, String whiteSpaces) {
-		if(s == null) {
-			return null;
-		}
-		return s.replaceAll("[" + whiteSpaces + "]+", " ");
-	}
-
-	public static boolean checkIfGoodEncoding(String s) {
-        if(s == null) {
-            return false;
-        }
-		return !Pattern.compile(StringUtility.WRONG_UNICODE).matcher(s).find();
-	}
-
-	public static String cleanToken(String s) {
-        if(s == null) {
-			return null;
-		}
-        s = StringUtility.normalizeWhiteSpaces(s);
-        s = StringUtility.singleWhiteSpaces(s);
-        s = StringUtility.trim(s, StringUtility.WHITE_SPACES);
-        if (s.startsWith("\"") && s.endsWith("\"")) {
-            s = StringUtility.trim(s, StringUtility.WHITE_SPACES + "\"");
-        }
-		return s;
+    public static String trim(final String s) {
+        return trim(s, StringUtility.WHITE_SPACES);
     }
 
-    public static String removeExtension(String fileName) {
-        if(fileName == null) {
+    public static String trim(final String s, final String whiteSpaces) {
+        if (s == null) {
             return null;
         }
-        return fileName.replaceFirst("[.][^.]+$", "");
+        return s.replaceAll("^[" + whiteSpaces + "]+", "").replaceAll("[" + whiteSpaces + "]+$", "");
     }
 
-    public static String ensureCamelStyle(String s) {
-        s = StringUtility.cleanToken(s.replaceAll("_", " "));
-        if (StringUtility.isEmpty(s)) {
+    public static String normalizeWhiteSpaces(final String s) {
+        return normalizeWhiteSpaces(s, StringUtility.WHITE_SPACES);
+    }
+
+    public static String normalizeWhiteSpaces(final String s, final String whiteSpaces) {
+        if (s == null) {
+            return null;
+        }
+        return s.replaceAll("[" + whiteSpaces + "]", " ");
+    }
+
+    public static String removeWhiteSpaces(final String s) {
+        return removeWhiteSpaces(s, StringUtility.WHITE_SPACES);
+    }
+
+    public static String removeWhiteSpaces(final String s, final String whiteSpaces) {
+        if (s == null) {
+            return null;
+        }
+        return s.replaceAll("[" + whiteSpaces + "]", "");
+    }
+
+    public static String singleWhiteSpaces(final String s) {
+        return singleWhiteSpaces(s, StringUtility.WHITE_SPACES);
+    }
+
+    public static String singleWhiteSpaces(final String s, final String whiteSpaces) {
+        if (s == null) {
+            return null;
+        }
+        return s.replaceAll("[" + whiteSpaces + "]+", " ");
+    }
+
+    public static boolean checkIfGoodEncoding(final String s) {
+        if (s == null) {
+            return false;
+        }
+        return !Pattern.compile(StringUtility.WRONG_UNICODE).matcher(s).find();
+    }
+
+    public static String cleanToken(final String s) {
+        if (s == null) {
+            return null;
+        }
+        String ss = StringUtility.normalizeWhiteSpaces(s);
+        ss = StringUtility.singleWhiteSpaces(ss);
+        ss = StringUtility.trim(ss, StringUtility.WHITE_SPACES);
+        if (ss.startsWith("\"") && ss.endsWith("\"")) {
+            ss = StringUtility.trim(ss, StringUtility.WHITE_SPACES + "\"");
+        }
+        return ss;
+    }
+
+    public static String ensureCamelStyle(final String s) {
+        // Consider _ seprated words instead of space 
+        String ss = StringUtility.cleanToken(s.replaceAll("_", " "));
+        if (StringUtility.isEmpty(ss)) {
             return "";
         }
 
         // Replace space by the next letter upper case
         boolean stillCamelToDo = true;
         while (stillCamelToDo) {
-            int i = s.indexOf(" ");
+            final int i = ss.indexOf(" ");
             if (i >= 0) {
-                s = s.substring(0, i) + Character.toUpperCase(s.charAt(i + 1)) + s.substring(i + 2);
+                ss = ss.substring(0, i) + Character.toUpperCase(ss.charAt(i + 1)) + ss.substring(i + 2);
             } else {
                 stillCamelToDo = false;
             }
         }
 
         // Ensure to start by a lower case
-        return Character.toLowerCase(s.charAt(0)) + s.substring(1);
+        return Character.toLowerCase(ss.charAt(0)) + ss.substring(1);
+    }
+
+    public static String capitalize(final String s) {
+        if (s == null) {
+            return null;
+        }
+        if (s.length() == 1) {
+            return s.toLowerCase();
+        }
+        else {
+            return Character.toUpperCase(s.charAt(0)) + s.substring(1).toLowerCase();
+        }
+    }
+
+    public static String removeExtension(final String fileName) {
+        if (fileName == null) {
+            return null;
+        }
+        return fileName.replaceFirst("[.][^.]+$", "");
     }
 }
