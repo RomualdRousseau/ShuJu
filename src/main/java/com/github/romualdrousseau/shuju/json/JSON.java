84b77c37257a27948e7e161a14b567b675fc7e19
Romuald Rousseau <romualdrousseau@gmail.com>
2023-01-02 10:33:59 +0800

    Cleanup

--- src/main/java/com/github/romualdrousseau/shuju/json/JSON.java
+++ src/main/java/com/github/romualdrousseau/shuju/json/JSON.java
@@ -5,10 +5,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
-import java.util.Arrays;
 import java.util.Iterator;
-import java.util.List;
-import java.util.regex.Pattern;
 import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
 
