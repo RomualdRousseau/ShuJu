14215d01817ebe1498a25645b727c22176eb5137
Romuald Rousseau <romuald.rousseau@servier.com>
2023-08-09 10:10:47 +0800

    Cleanup

--- src/main/java/com/github/romualdrousseau/shuju/util/CollectionUtils.java
+++ src/main/java/com/github/romualdrousseau/shuju/util/CollectionUtils.java
@@ -4,10 +4,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-import org.tensorflow.ndarray.Shape;
-import org.tensorflow.ndarray.buffer.DataBuffers;
-import org.tensorflow.types.TFloat32;
-
 public class CollectionUtils {
 
     public static List<Integer> mutableRange(int a, int b) {
@@ -21,13 +17,5 @@ public class CollectionUtils {
     public static <T> List<T> shuffle(List<T> l) {
         Collections.shuffle(l);
         return l;
-    }
-
-    public static TFloat32 ListOfIntegertoTFloat32(final List<Integer> l) {
-        final float[] result = new float[l.size()];
-        for (int i = 0, j = 0; i < l.size(); i++, j++) {
-            result[j] = (float) l.get(i);
-        }
-        return TFloat32.tensorOf(Shape.of(1, l.size()), DataBuffers.of(result));
     }
 }
