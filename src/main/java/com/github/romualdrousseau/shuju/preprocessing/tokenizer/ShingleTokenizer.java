4e1aa3bb9310569586cc2bd97c29988afff4ef07
Romuald Rousseau <romuald.rousseau@servier.com>
2023-08-16 20:34:23 +0800

    Version up

--- src/main/java/com/github/romualdrousseau/shuju/preprocessing/tokenizer/ShingleTokenizer.java
+++ src/main/java/com/github/romualdrousseau/shuju/preprocessing/tokenizer/ShingleTokenizer.java
@@ -9,13 +9,20 @@ import java.util.regex.Pattern;
 import com.github.romualdrousseau.shuju.preprocessing.Text;
 import com.github.romualdrousseau.shuju.util.StringUtils;
 
-public class ShingleTokenizer implements Text.ITokenizer{
+public class ShingleTokenizer implements Text.ITokenizer {
     private static final Pattern CAMEL_PATTERN = Pattern.compile("(?<!(^|[A-Z/]))(?=[A-Z/])|(?<!^)(?=[A-Z/][a-z/])");
+    private static final int MIN_SIZE = 2;
 
     private final Map<String, List<String>> variants;
+    private final int minSize;
 
     public ShingleTokenizer(final List<String> lexicon) {
+        this(lexicon, MIN_SIZE);
+    }
+
+    public ShingleTokenizer(final List<String> lexicon, final int minSize) {
         this.variants = Text.get_lexicon(lexicon);
+        this.minSize = minSize;
     }
 
     @Override
@@ -42,7 +49,7 @@ public class ShingleTokenizer implements Text.ITokenizer{
         final ArrayList<String> result = new ArrayList<String>();
         for (final String ss : s.split(" ")) {
             for (final String sss : CAMEL_PATTERN.split(ss)) {
-                if (sss.length() > 0 && (sss.length() > 1 || !Character.isAlphabetic(sss.charAt(0)))) {
+                if (sss.length() > 0 && (sss.length() > (minSize - 1) || !Character.isAlphabetic(sss.charAt(0)))) {
                     result.add(sss.toLowerCase());
                 }
             }
