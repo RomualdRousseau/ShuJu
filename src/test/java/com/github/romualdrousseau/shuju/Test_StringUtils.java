c3223c52f5b43a2c84466dac1ce2e72dad39adbc
Romuald Rousseau <romuald.rousseau@servier.com>
2024-07-16 17:23:49 +0800

    feat: Test lemmatization

--- src/test/java/com/github/romualdrousseau/shuju/Test_StringUtils.java
+++ src/test/java/com/github/romualdrousseau/shuju/Test_StringUtils.java
@@ -12,16 +12,44 @@ import com.github.romualdrousseau.shuju.strings.StringUtils;
 public class Test_StringUtils {
 
     @Test
-    public void testSnake() {
-        final var tokenizer = new ShingleTokenizer(List.of("total", "dollar", "percent"));
+    public void testSnakeWithLemmatization() {
+        final var tokenizer = new ShingleTokenizer(List.of("total,tot", "dollar", "percent"));
         assertEquals("total_quantity_dollar", StringUtils.toSnake("total quantity $", tokenizer));
         assertEquals("total_quantity_dollar", StringUtils.toSnake("TotalQuantity$", tokenizer));
         assertEquals("total_quantity_dollar", StringUtils.toSnake("totalquantity$", tokenizer));
+        assertEquals("total_quantity_dollar", StringUtils.toSnake("tot quantity $", tokenizer));
+        assertEquals("total_quantity_dollar", StringUtils.toSnake("TotQuantity$", tokenizer));
+        assertEquals("total_quantity_dollar", StringUtils.toSnake("totquantity$", tokenizer));
     }
 
     @Test
-    public void testCamel() {
-        final var tokenizer = new ShingleTokenizer(List.of("total", "dollar", "percent"));
+    public void testSnakeWithoutLemmatization() {
+        final var tokenizer = new ShingleTokenizer(List.of("total,tot", "dollar", "percent"), 1, false);
+        assertEquals("total_quantity_dollar", StringUtils.toSnake("total quantity $", tokenizer));
+        assertEquals("total_quantity_dollar", StringUtils.toSnake("TotalQuantity$", tokenizer));
+        assertEquals("total_quantity_dollar", StringUtils.toSnake("totalquantity$", tokenizer));
+        assertEquals("tot_quantity_dollar", StringUtils.toSnake("tot quantity $", tokenizer));
+        assertEquals("tot_quantity_dollar", StringUtils.toSnake("TotQuantity$", tokenizer));
+        assertEquals("tot_quantity_dollar", StringUtils.toSnake("totquantity$", tokenizer));
+    }
+
+    @Test
+    public void testCamelWithLemmatization() {
+        final var tokenizer = new ShingleTokenizer(List.of("total,tot", "dollar", "percent"));
+        assertEquals("totalQuantityDollar", StringUtils.toCamel("total quantity $", tokenizer));
+        assertEquals("totalQuantityDollar", StringUtils.toCamel("TotalQuantity$", tokenizer));
+        assertEquals("totalQuantityDollar", StringUtils.toCamel("totalquantity$", tokenizer));
+        assertEquals("totalQuantityDollar", StringUtils.toCamel("tot quantity $", tokenizer));
+        assertEquals("totalQuantityDollar", StringUtils.toCamel("TotQuantity$", tokenizer));
+        assertEquals("totalQuantityDollar", StringUtils.toCamel("totquantity$", tokenizer));
+    }
+
+    @Test
+    public void testCamelWithoutLemmatization() {
+        final var tokenizer = new ShingleTokenizer(List.of("total,tot", "dollar", "percent"), 1, false);
+        assertEquals("totQuantityDollar", StringUtils.toCamel("tot quantity $", tokenizer));
+        assertEquals("totQuantityDollar", StringUtils.toCamel("TotQuantity$", tokenizer));
+        assertEquals("totQuantityDollar", StringUtils.toCamel("totquantity$", tokenizer));
         assertEquals("totalQuantityDollar", StringUtils.toCamel("total quantity $", tokenizer));
         assertEquals("totalQuantityDollar", StringUtils.toCamel("TotalQuantity$", tokenizer));
         assertEquals("totalQuantityDollar", StringUtils.toCamel("totalquantity$", tokenizer));
